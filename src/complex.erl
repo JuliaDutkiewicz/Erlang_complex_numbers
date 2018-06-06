125f2a73f52d63b12df1c09a391fa1b90a31bb21
Mikołaj Pniak <mmpniak@gmail.com>
2018-06-06 17:13:33 +0200

    Added some functions in order to show QuickCheck functionality.

--- src/complex.erl
+++ src/complex.erl
@@ -10,7 +10,7 @@
 -author("Julia").
 
 %% API
--export([make_number/2, conjugate/1, add/2, multiply/2, modulus/1]).
+-export([make_number/2, conjugate/1, add/2, multiply/2, modulus/1, equals/2, arg_sin/1, arg_cos/1, arg/1]).
 
 -record(complex, {re,im}).
 
@@ -26,6 +26,22 @@ add(A, B) ->
 multiply(A, B) ->
   #complex{re = (A#complex.re * B#complex.re - A#complex.im * B#complex.im), im = (A#complex.im * B#complex.re + A#complex.re * B#complex.im) }.
 
-
 modulus(A) ->
   math:sqrt(A#complex.re * A#complex.re + A#complex.im * A#complex.im).
+
+% Function that compares two complex nums and resolves if they are equal, based on "equality of their real and imaginary parts.
+% ("Equality" means that one of them is not bigger than 0.1% of the smaller number)
+equals(A, B) ->
+  (abs(abs(A#complex.re) - abs(B#complex.re)) =<
+    abs(0.001*(min(abs(A#complex.re), abs(B#complex.re)))))
+    and ((abs(A#complex.im) - abs(B#complex.im)) =<
+    abs(0.001*(min(abs(A#complex.im), abs(B#complex.im))))).
+
+arg_sin(A) ->
+  A#complex.re / conjugate(A).
+
+arg_cos(A) ->
+  A#complex.im / conjugate(A).
+
+arg(A) ->
+  math:asin(arg_sin(A)).
--- src/complex_test.erl
+++ src/complex_test.erl
@@ -11,16 +11,39 @@
 -include_lib("eqc/include/eqc.hrl").
 
 %% API
--export([prop_square/0, prop_add/0]).
+-export([prop_square/0, prop_add/0, prop_sub/0, prop_multiply_args/0, prop_square_wrong/0, prop_square_fixed/0]).
 
 %(mod(z))^2 = z* con(z)
+% Checks if  z * con(z) = Re(z)^2 + Im(z)^2
 prop_square() ->
   ?FORALL({R,I},{int(),int()},
     complex:multiply(complex:make_number(R,I), complex:conjugate(complex:make_number(R,I))) == complex:make_number(R*R+I*I,0)).
 
+% Checks if (mod(z))^2 = z* con(z) using basic equality. May not always work because sqrt(x)^2 is not always x in the float arith!
+prop_square_wrong() ->
+  ?FORALL({R,I},{int(),int()},
+    complex:multiply(complex:make_number(R,I), complex:conjugate(complex:make_number(R,I))) == complex:make_number(complex:modulus(complex:make_number(R,I)) * complex:modulus(complex:make_number(R,I)),0)).
+
+% Checks if (mod(z))^2 = z* con(z) using approximated equality, defined in "complex" module.
+prop_square_fixed() ->
+  ?FORALL({R,I},{int(),int()},
+    complex:equals(
+      complex:multiply(complex:make_number(R,I), complex:conjugate(complex:make_number(R,I))),
+      complex:make_number(complex:modulus(complex:make_number(R,I)) * complex:modulus(complex:make_number(R,I)),0)
+  )).
+
+
+% z + con(z) = 2 Re(z)
 prop_add() ->
   ?FORALL({R,I},{int(),int()},
     complex:add(complex:make_number(R, I), complex:conjugate(complex:make_number(R,I))) == complex:make_number(2*R, 0)).
 
+% z + con(z) = 2i Im(z)
+prop_sub() ->
+  ?FORALL({R,I},{int(),int()},
+    complex:add(complex:make_number(R, I), complex:conjugate(complex:make_number(-R,-I))) == complex:make_number(0, 2*I)).
 
+prop_multiply_args() ->
+  ?FORALL({A,B,C,D},{int(),int(),int(),int()},
+    complex:arg(complex:multiply(complex:make_number(A,B), complex:make_number(C,D))) == complex:arg(complex:make_number(A,B)) + complex:arg(complex:make_number(C,D))).
 
